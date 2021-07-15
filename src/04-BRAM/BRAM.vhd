771cbce11913e4ecd42b7ed1d2fc4cdbd49f1972
Waine Junior <waine.oliveira.jr@gmail.com>
2021-07-14 22:24:23 -0300

    Minor refact

--- src/04-BRAM/BRAM.vhd
+++ src/04-BRAM/BRAM.vhd
@@ -3,15 +3,15 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 Entity BRAM is
-   PORT(RST       : in  std_logic;        
+    PORT(RST       : in  std_logic;        
         CLK       : in  std_logic;        
         READDATA  : out std_logic_vector(7 downto 0);
         WRITEDATA : in  std_logic_vector(7 downto 0);
         WR_EN     : in std_logic;           
         RD_EN     : in std_logic;            
         CS        : in std_logic;           
-        ADD       : in std_logic_vector (9 downto 0)
-		  );
+        ADD       : in std_logic_vector (9 downto 0) -- address
+        );
 end entity;
 
 
